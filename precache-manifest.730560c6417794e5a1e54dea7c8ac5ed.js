3bd9d2a0b84c152fbae1fa2bf3f7188d4cd6f954
josezanle <63666176+josezanle@users.noreply.github.com>
2020-08-21 14:37:01 -0300

    este repositorio es tarea del modulo 4 del programa PILIS de Catamarca, seccion de React js

---
+++ precache-manifest.730560c6417794e5a1e54dea7c8ac5ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2a427b0583f016910b8d80f21488500",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00c4a9100f72b773d5d6",
+    "url": "/static/css/main.14602da6.chunk.css"
+  },
+  {
+    "revision": "6bf3ab3c83e297b9cf40",
+    "url": "/static/js/2.691a5a87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.691a5a87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00c4a9100f72b773d5d6",
+    "url": "/static/js/main.d171ffd7.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
